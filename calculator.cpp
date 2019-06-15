18a2faa58026a8d90855b6b1e9dab5b019397243
amitdgr8 <amit_peace@live.com>
2019-06-15 08:25:18 +0000

    second

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std;
 int main()
-{   clear();
-    int x,y;
+{   
+    int x,y,op;
     cout << "Enter Two Numbers"<<endl;
     cin>>x>>y;
     cout<<"Choose the appopriate below option" << endl;
-    cout<<"1-> ADD\n2-> SUBTRACT\n3-> MULTIPLY\n4-> DIVISION";
+    cout<<"1-> ADD\n2-> SUBTRACT\n3-> MULTIPLY\n4-> DIVISION"<<endl;
+    cin>>op;
+    if(op==1){
+        cout<<x+y<<endl;
+    }else if(op==2){
+        cout<<x-y<<endl;
+    }else if(op==3){
+        cout<<x*y<<endl;
+    }else if(op==4){
+        cout<<x/y<<endl;
+    }else{
+        cout<<"Wrong Option Choosen .... Application Exiting";
+    }
     return 0;
 }
