c3390eac9819adc8970de8656fc18ee3fa3a1813
amitdgr8 <amit_peace@live.com>
2019-06-15 08:15:28 +0000

    second

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-    cout << "Hello, world!\n";
+{   clear();
+    int x,y;
+    cout << "Enter Two Numbers"<<endl;
+    cin>>x>>y;
+    cout<<"Choose the appopriate below option" << endl;
+    cout<<"1-> ADD\n2-> SUBTRACT\n3-> MULTIPLY\n4-> DIVISION";
     return 0;
 }
